6e1e62744afd5def7033aea94eba5904203842f3
Darwin Aguilar <darwinchilon@gmail.com>
2023-05-23 20:31:16 -0500

    creacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { DetalleHeroeComponent } from './components/detalle-heroe/detalle-heroe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home' , component: HomeComponent
+  },
+  {
+    path: 'about' , component: AboutComponent
+  },
+  {
+    path: 'heroes' , component: HeroesComponent
+  },
+  {
+    path: 'detalle' , component: DetalleHeroeComponent
+  }
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
