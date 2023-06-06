dbd3b12ca07ec559bdfaf1e6185d741b42b79c7e
Darwin Aguilar <darwinchilon@gmail.com>
2023-06-05 21:51:02 -0500

    creacion de servicio heroes y for

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Heroe } from 'src/app/models/heroes.interface';
+import { HeroeService } from 'src/app/services/heroe.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class HeroesComponent {
 
+  listarHeroes: Heroe[] = [];
+
+  constructor(
+    private heroeServices: HeroeService
+  ){
+    this.listarHeroes = this.heroeServices.getListarHeroes();
+  }
 }
